d8e30a38e997a75d381358b45a09124895f205dc
Aman Raj <amanraj68bittu@gmail.com>
2023-07-19 19:27:17 +0530

    adding_contact_login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { MemberComponent } from './member/member.component';
+import { SubjectComponent } from './subject/subject.component';
+import { ContactComponent } from './contact/contact.component';
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'member', component: MemberComponent},
+  {path: 'subject', component: SubjectComponent},
+  {path: 'contact', component: ContactComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { ContactComponent } from './contact/contact.component';
+import { MemberComponent } from './member/member.component';
+import { SubjectComponent } from './subject/subject.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    ContactComponent,
+    MemberComponent,
+    SubjectComponent
   ],
   imports: [
     BrowserModule,
